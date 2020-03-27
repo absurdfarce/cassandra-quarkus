ba3a20347250ab6432844791d479c399a82feb6c
tomekl007 <tomasz.lelek@datastax.com>
2020-03-27 12:41:37 +0100

    JAVA-2709 change package names to artifactId, use `maven-surefire-plugin.version`  3.0.0-M4 to start all Junit5 tests

---
+++ extensions/cassandra-client/runtime/src/main/java/com/datastax/oss/quarkus/config/CassandraClientConfig.java
@@ -0,0 +1,19 @@
+package com.datastax.oss.quarkus.config;
+
+import io.quarkus.runtime.annotations.ConfigItem;
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
+
+@ConfigRoot(name = CassandraClientConfig.CONFIG_NAME, phase = ConfigPhase.RUN_TIME)
+public class CassandraClientConfig {
+    public static final String CONFIG_NAME = "cassandra";
+
+    /**
+     * The cassandra client config.
+     */
+    @ConfigItem(name = ConfigItem.PARENT)
+    public CassandraClientConnectionConfig cassandraClientConnectionConfig;
+
+    // todo more low granular conf objects (i.e. ssl, cloud)
+
+}
---
+++ extensions/cassandra-client/runtime/src/main/java/com/datastax/oss/quarkus/config/CassandraClientConnectionConfig.java
@@ -0,0 +1,33 @@
+package com.datastax.oss.quarkus.config;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+
+import io.quarkus.runtime.annotations.ConfigGroup;
+import io.quarkus.runtime.annotations.ConfigItem;
+
+@ConfigGroup
+public class CassandraClientConnectionConfig {
+    /**
+     * Contact-points used to connect to Cassandra.
+     * If not specified, it will connect to local-host.
+     */
+    @ConfigItem(name = "contact-points", defaultValue = "127.0.0.1:9042")
+    public List<String> contactPoints;
+
+    /**
+     * Local data-center used when creating a {@link CqlSession}
+     */
+    @ConfigItem(name = "load-balancing-policy.local-datacenter")
+    public String localDataCenter;
+
+    /**
+     * How long the driver waits for a request to complete.
+     */
+    @ConfigItem(name = "request.timeout")
+    public Optional<Duration> requestTimeout;
+
+}
