4a1289e8a253c4dd415ae217808029de444a86d4
tomekl007 <tomasz.lelek@datastax.com>
2020-04-01 10:47:08 +0200

    JAVA-2696 add `@RegisterForReflection` on `TestNameConverter` to make native test pass

--- integration-tests/src/main/java/com/datastax/oss/quarkus/dao/nameconverters/TestNameConverter.java
+++ integration-tests/src/main/java/com/datastax/oss/quarkus/dao/nameconverters/TestNameConverter.java
@@ -3,7 +3,9 @@ package com.datastax.oss.quarkus.dao.nameconverters;
 import com.datastax.oss.driver.api.mapper.entity.naming.NameConverter;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 
+@RegisterForReflection
 public class TestNameConverter implements NameConverter {
 
     @Override
